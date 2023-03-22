f7e8be82d6d7f74e046ec381537e216b6e09270c
AlyssonH <61508945+veqis@users.noreply.github.com>
2023-03-22 10:52:22 -0300

    Update itens.py

--- itens.py
+++ itens.py
@@ -2,28 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-site = requests.get("https://darksouls.wiki.fextralife.com/Ammunition")
+site = requests.get("https://darksouls.wiki.fextralife.com/Ore")
 soup = BeautifulSoup(site.text, "html.parser")
 
-tabelaammo = soup.find("table", attrs={"class":"wiki_table"})
+tabela = soup.find("table", attrs={"class":"wiki_table"})
 
 corpo = soup.tbody
 
-def gravar(nome, fis, magi, fire, eletro, crit):
-    f = open("ammo.txt", "a")
-    f.writelines('{}, {}, {}, {}, {}, {}\n'.format(
-        nome, fis, magi, fire, eletro, crit))
+def gravar(nome, fis):
+    f = open("texto.txt", "a")
+    f.writelines("('{}', '{}'),\n".format(
+        nome, fis))
     f.close()
     
 corpo = corpo.findAll("tr")
 
 for i in range(1, len(corpo)):
-    titulo = corpo[i].find("strong")
-    print(titulo.text)
     tds = corpo[i].find_all("td")
+    titulo = tds[0].text
     dano_fisico = tds[1].text
-    print(dano_fisico)
-    
-  
-                       
-    
+
+    gravar(titulo,dano_fisico)
